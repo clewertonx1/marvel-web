b59d7ea550ff7b59388141158b4216134403c184
Clewerton Roque <clewerton203@gmail.com>
2020-12-28 17:31:48 -0300

    Schroll infinite

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef}from 'react'
 import getData from '../service/api'
 import Link from 'next/link'
-import {HiOutlineArrowCircleDown} from 'react-icons/hi'
-import { Button } from 'react-bootstrap';
 
 
 export default function Home() {
@@ -11,94 +9,56 @@ export default function Home() {
   const NavBar = () =>{
     return(
       <div onClick={() => nextPage()} id={"navbar"}>
-          <HiOutlineArrowCircleDown  id={'icon'}></HiOutlineArrowCircleDown>
+          <h1 id={'icon'}>More heros +</h1>
       </div>
     )
   }
 
-  const style = {
-    main:{
-      magin: 0,
-      display: 'flex',
-      flexDirection: "row",
-      alignItems: 'center',
-      backgroundColor: '#f71b39',
-      overflow: 'auto',
-      height: "100vh",
-    },
-    card:{
-      padding: 10,
-      display: 'block',
-      widht: 520, 
-      height: 520,
-      backgroundColor: '#dedede',
-      margin: 30,
-      borderRadius: 10,
-    },
-    descriptionDiv:{
-      margin: 20,
-    },
-    descriptionTextCard:{
-      marginTop: 5,
-      textAling: 'justify',
-      textJustify: "auto",
-      fontSize: 13,
-      color: "#424242"
-    },
-    titleCard:{
-      margin: 0,
-      fontSize: 25,
-      color: "#424242"
-    },
-    lastModifiedCard:{
-      margin: 0,
-      fontSize: 10,
-      color: "#424242"
-    },
-    title:{
-      marginBottom: 0,
-      fontSize: 50,
-      color: 'white',
-    },
-    subTitle:{
-      marginTop: 0,
-      fontSize: 30,
-      color: 'white',
-    }
-  }
-
+  const [showLoadign, setShowLoading] = useState(false)
 
   const [data, setData] = useState([])
 
   const [offset, setOffset] = useState(0)
 
+  const [schrollRadio, setSchrollRadio] = useState(null)
+
+  const schrollObserve = useRef()
+
   useEffect(() => {
-    console.log("uma vez")
-    async function setDataState(){
-      let a = await getData(offset)
-      setData(a)
+    const intersectionObserver = new IntersectionObserver((entries => {
+      const radio = entries[0].intersectionRatio
+      setSchrollRadio(radio)
+    }))
+
+    intersectionObserver.observe(schrollObserve.current)
+    
+    return () => {
+      intersectionObserver.disconnect()
     }
-    setDataState()
   },[])
+  
+
+ 
+
   useEffect(() => {
-    console.log("offset")
+    setShowLoading(true)
+    
     async function setDataState(){
-      let a = await getData(offset)
-      setData(data.concat(a))
+      let response = await getData(offset)
+      setData(data.concat(response))
     }
-    setDataState()
-  },[offset])
+
+    if(schrollRadio > 0 && data !== []){
+      nextPage()
+      setDataState()
+    }
+    setShowLoading(false)
+  },[schrollRadio])
 
   function nextPage(){
     setOffset(offset + 20)
   }
 
-  function backPage(){
-    if(offset === 0){
-      return
-    }
-    setOffset(offset - 20)
-  }
 
 
   return (
@@ -109,26 +69,28 @@ export default function Home() {
           <div id={"card"}>
             
               <img src={`${d.thumbnail.path}/standard_fantastic.${d.thumbnail.extension}`}></img>
-            
-            <div id={"description"}>
-              <p style={style.lastModifiedCard}>{d.modified.substring(0, 10)}</p>
-              <h1 style={style.titleCard}>{d.name}</h1>
+
+            <div id={"descritpionDiv"}>
+              <p id={"lastModified"}>{d.modified.substring(0, 10)}</p>
+              <h1 id={"title"}>{d.name}</h1>
               {d.description ?
-                <p style={style.descriptionTextCard}>{d.description}</p>
+                <p id={"descriptionText"}>{d.description}</p>
               : 
-                <p style={style.descriptionTextCard} >No have description</p>
+                <p id={"descriptionText"}></p>
               }
               <Link href={{ pathname: '/about', query: { id: d.id } }}>
-                <p style={{fontSize: 15,marginBottom: 0, color: '#f71b39'}}>Find out more</p>
+                <h2  id={"findMore"}>Find out more</h2>
               </Link>
             </div>
           </div>  
-        ) 
-      })}
-      </div>
-   
-        <NavBar></NavBar>
-
+        )})}
+      </div> 
+      <div ref={schrollObserve}></div>
+      {showLoadign ? 
+        <h1>loadding</h1> 
+      :
+        <h1></h1>
+      }
     </div>
   )
 }
