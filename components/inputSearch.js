4e19beca6397b0f0c335846661886094de97f173
Clewerton Roque <clewerton203@gmail.com>
2020-12-29 13:34:24 -0300

    search

--- components/inputSearch.js
+++ components/inputSearch.js
@@ -1,9 +1,16 @@
 import React from "react"
+import {AiOutlineSearch} from 'react-icons/ai'
+
 
 const InputSearch = (props) => (
-  <input className={"search"}>
-  
+  <div className={"search"}>
+  <input className={"input"}>
+    
   </input>
+  <div className={"iconDivSearch"}>
+    <AiOutlineSearch></AiOutlineSearch>
+  </div>
+  </div>
 )
 
 export default InputSearch
